f214badfed79bcd2d869b6e5620cc733d96c8115
Dharmendra <Dharmendra@192.168.1.4>
2017-02-22 20:27:58 +0530

    formula commit

---
+++ Formula.java
@@ -0,0 +1,23 @@
+package com.main.hacker_rank.loops;
+
+import java.util.Scanner;
+
+public class Formula {
+	
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(n);
+        }
+        in.close();
+    }
+
+}
